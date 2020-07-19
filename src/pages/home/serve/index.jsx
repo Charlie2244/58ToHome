6cc5f9d5f24f84bc32bf9c46b15efa86e21543ab
Charlie2244 <lhs2244@163.com>
2020-07-19 08:33:11 +0800

    base

---
+++ src/pages/home/serve/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css'
+import Detail from './detail'
+import Header from '../../../components/header/header'
+export default function Serve(){
+    return(
+        <div >
+            <Header />
+            <Detail />
+        </div>
+    )
+}
